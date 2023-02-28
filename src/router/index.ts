a0ddc5054463d2c78c9dda514be80b22d23484fa
ZombieBrand <zombiebrandg@gmail.com>
2023-02-28 20:42:33 +0800

    feat: add router

---
+++ src/router/index.ts
@@ -0,0 +1,10 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import HelloWorldVue from "../components/HelloWorld.vue";
+const routes = [
+  { path: "/", component: HelloWorldVue},
+];
+
+export const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
